d0332e7b431d313f58ee488b9c27bbad9b1563bf
bachtung2003 <tranbachtungnvc@gmail.com>
2024-06-08 02:33:06 +0700

    complete project

--- src/components/TaskInput/TaskInput.tsx
+++ src/components/TaskInput/TaskInput.tsx
@@ -1,31 +1,51 @@
 import React, { useState } from 'react'
 import styles from './taskInput.module.scss'
+import { Todo } from '../../@types/todo.type'
 
 interface TaskInputProps {
   addTodo: (name: string) => void
+  currentTodo: Todo | null
+  editTodo: (name: string) => void
+  finishEditTodo: () => void
 }
 
 const TaskInput = (props: TaskInputProps) => {
-  const { addTodo } = props
+  const { addTodo, currentTodo, editTodo, finishEditTodo } = props
   const [name, setName] = useState<string>('')
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    addTodo(name)
-    setName('')
+    if (currentTodo && currentTodo.name !== '') {
+      finishEditTodo()
+      if (name) setName('')
+    } else {
+      if (name !== '' && currentTodo?.name !== '') {
+        addTodo(name)
+        setName('')
+      }
+    }
   }
 
   const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target
-    setName(value)
+    if (currentTodo) {
+      editTodo(value)
+    } else {
+      setName(value)
+    }
   }
 
   return (
     <div>
       <h1 className={styles.title}>TS To do list</h1>
       <form className={styles.form} onSubmit={handleSubmit}>
-        <input type='text' placeholder='caption goes here' value={name} onChange={onChangeInput} />
-        <button type='submit'>➕</button>
+        <input
+          type='text'
+          placeholder='caption goes here'
+          value={currentTodo ? currentTodo.name : name}
+          onChange={onChangeInput}
+        />
+        <button type='submit'>{currentTodo ? '✓' : '➕'}</button>
       </form>
     </div>
   )
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -7,6 +7,8 @@ import { Todo } from '../../@types/todo.type'
 const TodoList = () => {
   const [todos, setTodos] = useState<Todo[]>([])
 
+  const [currentTodo, setCurrentTodo] = useState<Todo | null>(null)
+
   const completed = todos.filter((todo) => todo.done)
   const incomplete = todos.filter((todo) => !todo.done)
 
@@ -30,13 +32,64 @@ const TodoList = () => {
     })
   }
 
+  const startEditTodo = (id: string) => {
+    const findedTodo = todos.find((todo) => todo.id === id)
+    if (findedTodo) setCurrentTodo(findedTodo)
+  }
+
+  const editTodo = (name: string) => {
+    setCurrentTodo((prev) => {
+      if (prev) return { ...prev, name }
+      return null
+    })
+  }
+
+  const finishEditTodo = () => {
+    setTodos((prev) => {
+      return prev.map((todo) => {
+        if (todo.id === (currentTodo as Todo).id) {
+          return currentTodo as Todo
+        }
+        return todo
+      })
+    })
+    setCurrentTodo(null)
+  }
+
+  const deleteTodo = (id: string) => {
+    if (currentTodo) {
+      setCurrentTodo(null)
+    }
+    setTodos((prev) => {
+      const findedIndexTodo = prev.findIndex((todo) => todo.id === id)
+      if (findedIndexTodo > -1) {
+        const result = [...prev]
+        result.splice(findedIndexTodo, 1)
+        return result
+      }
+      return prev
+    })
+  }
+
   console.log(todos)
   return (
     <div className={styles.todoList}>
       <div className={styles.todoListContainer}>
-        <TaskInput addTodo={addTodo} />
-        <TaskList doneTaskList={false} todos={incomplete} handleDoneTodo={handleDoneTodo} />
-        <TaskList doneTaskList={true} todos={completed} handleDoneTodo={handleDoneTodo} />
+        <TaskInput addTodo={addTodo} currentTodo={currentTodo} editTodo={editTodo} finishEditTodo={finishEditTodo} />
+        <TaskList
+          doneTaskList={false}
+          todos={incomplete}
+          handleDoneTodo={handleDoneTodo}
+          startEditTodo={startEditTodo}
+          deleteTodo={deleteTodo}
+        />
+        <TaskList
+          doneTaskList={true}
+          todos={completed}
+          handleDoneTodo={handleDoneTodo}
+          startEditTodo={startEditTodo}
+          deleteTodo={deleteTodo}
+        />
       </div>
     </div>
   )
